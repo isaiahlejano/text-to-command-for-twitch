df40dd0c87647865a65ab33c8f75881b131134f4
John Isaiah Lejano <isaiahlejano.professional@gmail.com>
2022-10-28 12:46:15 +0800

    added jquery and twitch api connection script

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const WebSocketClient = require('websocket').client;
+const client = new WebSocketClient();
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+    console.log('WebSocket Client Connected');
+    
+    // Send CAP (optional), PASS, and NICK messages
+});
+
+client.connect('ws://irc-ws.chat.twitch.tv:80');
